d9341f624df020fae418e74dba24c6dd83d462f6
Web Dev <app.dev0528@gmail.com>
2019-01-18 17:03:27 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(logger('dev')); // Log requests to API using morgan
 // Enable CORS from client-side
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://alfonsoserver.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
